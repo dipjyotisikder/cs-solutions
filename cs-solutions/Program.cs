25ac9509b81ceda7f516e11b3d28f9371d95a897
Dip Sikder <dip@mailinator.com>
2021-08-03 04:46:45 +0600

    init

---
+++ cs-solutions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_solutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
